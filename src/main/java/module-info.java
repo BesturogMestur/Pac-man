20ffb56d0d088b550b20951b577bbcce97422a10
BesturogMestur <birgirsig02@gmail.com>
2023-03-23 10:52:39 +0000

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module hi.hbv201g.vidmot {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens hi.hbv201g.vidmot to javafx.fxml;
+    exports hi.hbv201g.vidmot;
+}
