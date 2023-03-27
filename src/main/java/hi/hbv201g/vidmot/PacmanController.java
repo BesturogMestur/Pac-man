178228beb095fa687964121d56b2e83d3d9ce27d
manifreyr <manifreyrr@gmail.com>
2023-03-27 18:04:55 +0000

    Orvatakkar

--- src/main/java/hi/hbv201g/vidmot/PacmanController.java
+++ src/main/java/hi/hbv201g/vidmot/PacmanController.java
@@ -1,14 +1,29 @@
 package hi.hbv201g.vidmot;
 
+import hi.hbv201g.vinsla.Stefna;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+import java.util.HashMap;
 
 public class PacmanController {
     @FXML
     private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+    private HashMap<KeyCode, Stefna> map = new HashMap<KeyCode, Stefna>();
+
+    public void orvatakkar(){
+        map.put(KeyCode.RIGHT, Stefna.HAEGRI);
+        map.put(KeyCode.LEFT, Stefna.VINSTRI);
+        map.put(KeyCode.UP, Stefna.UPP);
+        map.put(KeyCode.DOWN, Stefna.NIDUR);
+    }
+    public void initialize(){
+
+
+
     }
 }
