e99b92ffa5bc4d7c01032261f8af6a7606cf9564
BesturogMestur <birgirsig02@gmail.com>
2023-04-13 10:35:31 +0000

    bæti við pellest í maze

--- src/main/java/hi/hbv201g/vidmot/PacmanMaze.java
+++ src/main/java/hi/hbv201g/vidmot/PacmanMaze.java
@@ -4,6 +4,9 @@ import hi.hbv201g.vinnsla.Blinky;
 import hi.hbv201g.vinnsla.Clyde;
 import hi.hbv201g.vinnsla.Inky;
 import hi.hbv201g.vinnsla.Pinky;
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.layout.GridPane;
 
@@ -29,16 +32,21 @@ public class PacmanMaze extends GridPane {
             {false, true, false, true, false, false, false, false, false, true, false, true, false},
             {false, true, true, true, true, true, true, true, true, true, true, true, false},
             {false, false, false, false, false, false, false, false, false, false, false, false, false}};
+    private final int[] TIMAR = {10,8,10,2};
+    private int timi;
     @FXML
     Pacman fxPacman;
     private Blinky blinky;
     private Inky inky;
     private Pinky pinky;
     private Clyde clyde;
+    private ObservableList<Pellets> pellets = FXCollections.observableArrayList();
 
     public PacmanMaze() {
         FXMLLoder maze = new FXMLLoder(this, FILE_NAME);
+        timi = TIMAR[0];
         setDraugar();
+        setPellets();
     }
 
     private void setDraugar() {
@@ -49,11 +57,24 @@ public class PacmanMaze extends GridPane {
 
     }
 
+    public void setPellets() {
+        for(int i =1; i<MAZE.length-1;i++){
+            for(int j =1; j<MAZE.length-1;j++){
+                if(MAZE[i][j]){
+                    Pellets p = new Pellets();
+                    pellets.add(p);
+                    add(p,i,j);
+                }
+            }
+        }
+    }
+
     public void aframDraugar() {
         aframDurgar(blinky);
         aframDurgar(inky);
         aframDurgar(pinky);
         aframDurgar(clyde);
+        if
     }
 
     private void aframDurgar(Draugar d) {
@@ -61,6 +82,15 @@ public class PacmanMaze extends GridPane {
         boolean[] walls = new boolean[4];
         a[0] = getColumnIndex(d);
         a[1] = getRowIndex(d);
+
+        if(timi<=0){
+            if(d.elta){
+                d.setElta(false);
+            }else {
+                d.setElta(true);
+            }
+        }
+
         for(int i = 0; i < walls.length;i++){
             if (i%2==0){
                 if(i==0){
