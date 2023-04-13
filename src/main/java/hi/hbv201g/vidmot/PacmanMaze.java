211df8a29a57a98323c7b9e6b62bdd0141f0da34
BesturogMestur <birgirsig02@gmail.com>
2023-04-13 09:58:16 +0000

    draugarninr gerta núna fræt sig um fylkið

--- src/main/java/hi/hbv201g/vidmot/PacmanMaze.java
+++ src/main/java/hi/hbv201g/vidmot/PacmanMaze.java
@@ -13,7 +13,22 @@ public class PacmanMaze extends GridPane {
     private final double[] UPPHAFS_PUNKTUR = {0, 0};
     private final double[] MESTA_LEGNT_FRA_UPPHAF = {getColumnCount(), getRowCount()};
     private final double[] BLINKY_HOME = {getColumnCount(), 0};
-    private final double[] CLYDE_HOME = {0,getRowCount()};
+    private final double[] CLYDE_HOME = {0, getRowCount()};
+    private final boolean[][] MAZE = {{false, false, false, false, false, false, false, false, false, false, false,
+            false, false},
+            {false, true, true, true, true, true, true, true, true, true, true, true, false},
+            {false, true, false, false, false, false, true, false, false, false, false, true, false},
+            {false, true, false, true, true, true, true, true, true, true, false, true, false},
+            {false, true, false, true, false, false, false, false, false, true, false, true, false},
+            {false, true, true, true, true, true, true, true, true, true, true, true, false},
+            {false, true, false, true, false, false, false, false, false, true, false, true, false},
+            {true, true, false, true, false, false, false, false, false, true, false, true, true},
+            {false, true, false, true, false, false, false, false, false, true, false, true, false},
+            {false, true, true, true, false, false, false, false, false, true, true, true, false},
+            {false, true, false, true, true, true, true, true, true, true, false, true, false},
+            {false, true, false, true, false, false, false, false, false, true, false, true, false},
+            {false, true, true, true, true, true, true, true, true, true, true, true, false},
+            {false, false, false, false, false, false, false, false, false, false, false, false, false}};
     @FXML
     Pacman fxPacman;
     private Blinky blinky;
@@ -28,11 +43,44 @@ public class PacmanMaze extends GridPane {
 
     private void setDraugar() {
         blinky = new Blinky(fxPacman, UPPHAFS_PUNKTUR, MESTA_LEGNT_FRA_UPPHAF, HOME, BLINKY_HOME);
-        inky = new Inky(fxPacman, UPPHAFS_PUNKTUR, MESTA_LEGNT_FRA_UPPHAF, HOME,MESTA_LEGNT_FRA_UPPHAF,blinky);
-        pinky = new Pinky(fxPacman, UPPHAFS_PUNKTUR, MESTA_LEGNT_FRA_UPPHAF, HOME,UPPHAFS_PUNKTUR);
+        inky = new Inky(fxPacman, UPPHAFS_PUNKTUR, MESTA_LEGNT_FRA_UPPHAF, HOME, MESTA_LEGNT_FRA_UPPHAF, blinky);
+        pinky = new Pinky(fxPacman, UPPHAFS_PUNKTUR, MESTA_LEGNT_FRA_UPPHAF, HOME, UPPHAFS_PUNKTUR);
         clyde = new Clyde(fxPacman, UPPHAFS_PUNKTUR, MESTA_LEGNT_FRA_UPPHAF, HOME, CLYDE_HOME);
 
     }
 
-}
+    public void aframDraugar() {
+        aframDurgar(blinky);
+        aframDurgar(inky);
+        aframDurgar(pinky);
+        aframDurgar(clyde);
+    }
+
+    private void aframDurgar(Draugar d) {
+        int[] a = new int[2];
+        boolean[] walls = new boolean[4];
+        a[0] = getColumnIndex(d);
+        a[1] = getRowIndex(d);
+        for(int i = 0; i < walls.length;i++){
+            if (i%2==0){
+                if(i==0){
+                    walls[i]=MAZE[a[0]+1][a[1]];
+                }else {
+                    walls[i]=MAZE[a[0]-1][a[1]];
+                }
+
+            }else {
+                if(i==1){
+                    walls[i]=MAZE[a[0]][a[1]+1];
+                }else {
+                    walls[i]=MAZE[a[0]][a[1]-1];
+                }
+                }
+            }
+        d.afarm(walls);
+        }
+
+    }
+
+
 
