3e5f1179c59ee6f5d39bdaf634ef1a0dd0ddf400
BesturogMestur <birgirsig02@gmail.com>
2023-04-15 22:27:30 +0000

    var laga

--- src/main/java/hi/hbv201g/vidmot/Draugar.java
+++ src/main/java/hi/hbv201g/vidmot/Draugar.java
@@ -17,7 +17,6 @@ public abstract class Draugar extends Circle implements Afarm, Hnit {
     public abstract double drauaReikniritd(double[] a);
 
     public Draugar(Pacman p, double[] a, double[] b, double[] home) {
-        FXMLLoder loader = new FXMLLoder(this,"Draugar.fxml");
         this.p = p;
         MAX_LEND = reknirit(a, b);
         HOME = home;
