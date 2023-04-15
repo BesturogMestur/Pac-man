1e4b5e648938a512f21b5cffe1a1954db5c4e46f
helena stefansdottir <hel>
2023-04-15 21:55:11 +0000

    eth

--- src/main/java/hi/hbv201g/vidmot/PacmanMaze.java
+++ src/main/java/hi/hbv201g/vidmot/PacmanMaze.java
@@ -2,7 +2,6 @@ package hi.hbv201g.vidmot;
 
 import hi.hbv201g.vinnsla.*;
 import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.geometry.HPos;
--- src/main/java/hi/hbv201g/vinnsla/Stefna.java
+++ src/main/java/hi/hbv201g/vinnsla/Stefna.java
@@ -2,7 +2,9 @@ package hi.hbv201g.vinnsla;
 
 public enum Stefna {
     NIDUR(270), HAEGRI(0), VINSTRI(180), UPP(90);
-    private int stefna;
+
+    private final int stefna;
+
     Stefna(int stefna) {
         this.stefna = stefna;
     }
